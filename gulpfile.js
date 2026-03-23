e6fb4d32c23da8c991a27ec277d3b434199f56f7
Lekseit <lekseir88@gmail.com>
2026-03-23 23:59:28 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,23 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+
+function optimizeImages() {
+    return gulp.src('src/images/**/*.{jpg,jpeg,png,gif,svg}', { encoding: false })
+        .pipe(imagemin([
+            imagemin.mozjpeg({ quality: 80 }),
+            imagemin.optipng({ optimizationLevel: 1 }),
+            imagemin.svgo(),
+            imagemin.gifsicle()
+        ], {
+            verbose: true
+        }))
+        .on('error', function(err) {
+            console.error('Ошибка оптимизации изображения:', err);
+            this.emit('end');
+        })
+        .pipe(gulp.dest('dist/images'));
+}
+
+exports.optimizeImages = optimizeImages;
+exports.default = optimizeImages;
 
